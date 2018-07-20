91a4b7e32f64fb9ec6bd472b5185fbc30b93c09e
Michael Scherer <misc@redhat.com>
2018-07-21 01:26:05 +0200

    Make flake8 happy on latest release

--- requests_saml/__init__.py
+++ requests_saml/__init__.py
@@ -3,7 +3,7 @@ TODO
 """
 # code under the general public license v3 or above
 # (c) 2018 Michael Scherer <mscherer@redhat.com>
-from .saml import HTTPSAMLAuth
+from .saml import HTTPSAMLAuth  # noqa: F401
 
 __all__ = ('HTTPSamlAuth')
 __version__ = '0.1'
