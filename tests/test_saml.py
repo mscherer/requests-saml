85478dd3d46fdc0e29e7dc3a57b8315025f412fb
Michael Scherer <misc@zarb.org>
2018-07-21 02:54:43 +0200

    Make the tests flake8 compliant

--- tests/test_saml.py
+++ tests/test_saml.py
@@ -1,9 +1,12 @@
 from requests_saml.saml import get_action, get_value
 
+
 def test_get_action():
-    string = '<HTML><BODY><FORM METHOD="POST" ACTION="foo"></FORM></BODY></HTML>'
-    assert get_action("foobar") == None
+    string = """<HTML><BODY><FORM METHOD="POST" ACTION="foo">
+    </FORM></BODY></HTML>"""
+    assert get_action("foobar") is None
     assert get_action(string) == "foo"
+
 
 def test_get_value():
     string = """<HTML><BODY>
@@ -11,5 +14,5 @@ def test_get_value():
     <INPUT NAME="%s" TYPE="HIDDEN" VALUE="bar" />
     </FORM>
     </BODY></HTML>""" % 'SAMLThing'
-    assert get_value("foobar", "SAMLThing") == None
+    assert get_value("foobar", "SAMLThing") is None
     assert get_value(string, "SAMLThing") == "bar"
